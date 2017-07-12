e0dd0e0a9fbe27dee64f43c476aaa6f2313fce70
nderim <nderim.rrahmani@gmail.com>
2017-07-12 13:30:22 +0200

    Initial project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+  <head>
+      <title>AFM</title>
+  </head>
+
+     <body>
+<h1>Hello from AFM <?= $name; ?> </h1>
+     </body>
+</html>
