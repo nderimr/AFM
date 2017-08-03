7e81f62455ccbb2a29d304ebaf21aa98d0a68d10
nderim <nderim.rrahmani@gmail.com>
2017-08-03 23:33:32 +0200

    remove query builder seed

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -13,17 +13,5 @@ class ArticlesTableSeeder extends Seeder
     public function run()
     {
       $article = factory(App\Article::class,10)->create();
-
-       /* $content='';
-        for($i=0;$i<220;$i++)
-            $content=$content.' '.str_random(7); 
-        
-        DB::table('articles')->insert([
-            'title' => str_random(20),
-            'content' => $content,
-            'type' => str_random(20),
-            'workflowName' => str_random(20),
-            'state' => str_random(5),
-        ]);*/
     }
 }
